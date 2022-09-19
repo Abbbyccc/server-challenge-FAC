3ffc5123fee7d64d9bc23973a0e5726f2bb9b298
Abbbyccc <abbychien@hotmail.com>
2022-09-19 17:13:35 +0100

    challenge 4 & 5

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const server = express();
 
 module.exports = server;
@@ -20,7 +19,7 @@ server.get("/", (req, res) => {
   `);
 });
 
-//Challenge 2
+//Challenge 2 & 3
 server.get("/colour", (req, res) => {
   const hex = req.query.hex;
   const hexColor = hex || "FFFFFF"
@@ -46,4 +45,36 @@ server.get("/colour", (req, res) => {
 `);
 });
 
-//Challenge 3
+//Challenge 4 & 5
+const cheeseList = []
+server.get("/cheese", (req, res) => {
+  const chessRating = cheeseList.map(({ name, rating }) =>
+    `<li>Name: ${name}  //  Rating: ${rating}</li>`);
+  res.send(`
+  <!doctype html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>Cheese</title>
+      <style>
+      </style>
+      </head>
+      <body>
+          <form action="/cheese" method="post">
+              <label for="name">Cheese Name:</label>
+              <input type="text" id="name" name="name"><br><br>
+              <label for="rating">Cheese Rating:</label>
+              <input type="range" id="rating" name="rating" min="0" max="5"><br><br>
+              <button type="submit">Rate Cheese</button>
+            </form>
+            <ul>${chessRating.join("")}</ul>
+      </body>
+      </html>
+`);
+});
+
+const bodyParser = express.urlencoded();
+server.post("/cheese", bodyParser, (req, res) => {
+  cheeseList.push(req.body)
+  res.redirect('/cheese');
+});
